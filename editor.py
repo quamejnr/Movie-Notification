29fb4dceb11fc137f4817b603f3aa5449a0ecb13
Kwame Akuamoah-Boateng <kaboateng14@yahoo.com>
2021-11-11 20:13:48 +0000

    Feat: Create desktop listener that notifies when a new movie is released.

--- editor.py
+++ editor.py
@@ -1,15 +1,10 @@
 from event import notify
-from api import YTSApi
 
-yts = YTSApi()
 
 TV_SHOWs = {
     'Cursed': ['cursed.s01e01', 'cursed.s01e02', 'cursed.s01e03', 'cursed.s01e04'],
     'The Resident': ['the.resident.s01e01', 'the.resident.s01e02', 'the.resident.s01e03', 'the.resident.s01e04']
 }
-
-
-MOVIES = ['Old Guard', 'Free Guy', 'Avengers', 'Marvel Eternals']
 
 
 def check_for_new_episode(series, episode):
@@ -20,12 +15,8 @@ def check_for_new_episode(series, episode):
     TV_SHOWs[series].append(episode)
 
 
-def check_for_new_movie(movies: list):
-    movie_urls = [yts.get_movie_url(movie) for movie in movies if yts.get_movie_url(movie) is not None]
-    if movie_urls:
-        notify()
-    return movie_urls
-
-arr = ['Shang-Chi', 'eternals']
-
-print(check_for_new_movie(arr))
+def check_for_new_movie(movies: list, movie_site):
+    for movie in movies:
+        movie_info = movie_site.get_movie_info(movie)
+        if movie_info:
+            notify('new_movie', movie_info)
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from listener import EmailListener, PhoneListener
 from desktop_listener import DesktopListener
 from editor import check_for_new_episode, check_for_new_movie
+from api import YTSApi
 
 
-# Initialize event listeners
-EmailListener()
-PhoneListener()
-DesktopListener()
+if __name__ == "__main__":
 
-# check for new episode
-check_for_new_episode('See', 'see.s01e01')
+    # Initialize event listeners
+    EmailListener()
+    PhoneListener()
+    DesktopListener()
 
-# check for new movie
-check_for_new_movie('The Green Knight')
+    # Initialize Movie Site
+    yts = YTSApi()
+
+    # MOVIES
+    MOVIES = ['shang-chi']
+
+    # check for new episode
+    # check_for_new_episode('See', 'see.s01e01')
+
+    # check for new movie
+    check_for_new_movie(MOVIES, movie_site=yts)
 
 
 
